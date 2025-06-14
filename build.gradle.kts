cec36ea7802d99961c1f26309d670ef9736bb28c
Maha-moni123 <mahalakshmip625@gmail.com>
2025-06-15 00:27:26 +0530

    "initial commit "

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false // Keep this as it is
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.0")
+        classpath("com.google.gms:google-services:4.4.2") // Ensure this matches your plugin version
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // If you need other repositories like jcenter(), you can add them here
+    }
+}
+
+rootProject.name = "GREENAURA"
+include(":app")
